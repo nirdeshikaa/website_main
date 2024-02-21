f83755d859ef38981fe24d01ad81f23c00e9aa6e
Nirdeshika <nirdeshikapandey572@gmail.com>
2024-02-21 16:42:50 +0545

    Framework Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Products from './components/Products/Products';
 import TopProducts from './components/TopProducts/TopProducts';
 import Banner from './components/Banner/Banner';
 import Subscribe from './components/Subscribe/Subscribe';
-import Testimonials from './components/Testimonials/Testimonials';
+
 import Footer from './components/Footer/Footer';
 
 import AOS from "aos";
@@ -30,7 +30,7 @@ const App = () => {
       <Banner />
       <Subscribe />
       <Products />
-      <Testimonials />
+      
       <Footer />
 
     </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Kathmandu, Nepal</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+977-0000000000</p>
                 </div>
               </div>
             </div>
