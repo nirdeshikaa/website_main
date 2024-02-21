82b97cc5f7be9983a703e69cc090c3d21b0a8e2d
Nirdeshika <nirdeshikapandey572@gmail.com>
2024-02-21 16:32:59 +0545

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
-import Product from './components/Products/Products';
+import Products from './components/Products/Products';
 import TopProducts from './components/TopProducts/TopProducts';
+import Banner from './components/Banner/Banner';
+import Subscribe from './components/Subscribe/Subscribe';
+import Testimonials from './components/Testimonials/Testimonials';
+import Footer from './components/Footer/Footer';
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -21,8 +25,14 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
-      <Product />
+      <Products />
       <TopProducts />
+      <Banner />
+      <Subscribe />
+      <Products />
+      <Testimonials />
+      <Footer />
+
     </div>
   )
 };
