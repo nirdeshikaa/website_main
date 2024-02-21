5dd170b450ae76311004cdfbddbd0d3dd19335e8
Nirdeshika <nirdeshikapandey572@gmail.com>
2024-02-21 16:06:00 +0545

    Product section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
-
+import Product from './components/Products/Products';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <Navbar />
       <Hero />
+      <Product />
     </div>
   )
 };
